4099e02b777f599bbf8f6b6dae721ff66fa2f99c
Piotr Mlynarski <Piotrek@MacBook-Pro-Piotr.local>
2015-02-12 14:57:12 +0100

    Initial Commit

---
+++ Kurwica/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Kurwica
+//
+//  Created by Piotr Mlynarski on 12.02.2015.
+//  Copyright (c) 2015 Piotr Mlynarski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
